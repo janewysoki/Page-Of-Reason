4f4b8ddcafac26476b5daf1bf609c90a1f7648c3
janewysoki <jane.wysoki@gmail.com>
2020-04-07 16:24:47 -0400

    Create user show page and user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
