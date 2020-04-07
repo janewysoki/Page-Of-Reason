c51a83c38f1ab011437df5153bacd3f1598f29df
janewysoki <jane.wysoki@gmail.com>
2020-04-07 16:26:50 -0400

    Enable bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :books # that the user has created
     #has_many :reviewed_books, through :reviews, source: :book
-   
+   has_secure_password
 end
