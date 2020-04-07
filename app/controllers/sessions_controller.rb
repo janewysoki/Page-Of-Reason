d3004342a7a8143d5e604734bb4de1c15e299791
janewysoki <jane.wysoki@gmail.com>
2020-04-07 17:09:20 -0400

    Add logout link to application layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
     def index
     end
+
+    def destroy
+        session[:user_id].destroy
+        redirect_to '/'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
+  delete '/logout' => 'sessions#destroy'
   resources :reviews
   resources :books
   resources :authors
