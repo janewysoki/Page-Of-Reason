e3acab3f640c1e1fbe7227a3160c8da3f5019f83
janewysoki <jane.wysoki@gmail.com>
2020-04-07 13:23:28 -0400

    Build sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :books # that the user has created
-    has_many :reviewed_books, through :reviews, source: :book
+    #has_many :reviewed_books, through :reviews, source: :book
    
 end
