4985176ef3eb184a1ea02cf91411560107b8dd13
janewysoki <jane.wysoki@gmail.com>
2020-04-09 13:50:17 -0400

    Revise reviews new action and view page to check for params and nested routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,13 @@
 class ReviewsController < ApplicationController
 
     def new
-        @book = Book.find_by_id(params[:book_id])
+        #if it's nested, do it this way
+        if @book = Book.find_by_id(params[:book_id])
         #this is nested route so we want review to already know about book it's associated with
-        @review = @book.reviews.build #(reviews.build instead of build.review beacuse a book doesn't belong to a review)
+            @review = @book.reviews.build #(reviews.build instead of build.review beacuse a book doesn't belong to a review)
+        else #if it's not nested
+            @review = Review.new
+        end
     end
 
     def create
