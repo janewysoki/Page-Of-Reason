6caa81c48c1436c82de95233ae7c4d87cb7af2a7
janewysoki <jane.wysoki@gmail.com>
2020-04-09 12:56:36 -0400

    Create validations for review class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :title, presence: true
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than 11} #prevent someone from giving it 100 instead of 1-10
 end
