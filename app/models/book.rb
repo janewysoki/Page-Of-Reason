d153d11ff5046abefbb45ed23bfcb4e3a1380fda
janewysoki <jane.wysoki@gmail.com>
2020-04-10 14:53:28 -0400

    Add validation and method to avoid duplicates to book class

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,11 +6,22 @@ class Book < ApplicationRecord
   accepts_nested_attributes_for :author
 
   validates :title, presence: true
-  
+  validate :no_duplicates
+
+  #difference between validate and validateS?
+
   def author_attributes=(attributes)
    self.author = Author.find_or_create_by(attributes) if !attributes['name'].empty? #NEED EXPLANATION //  SHOULD I USE :NAME INSTEAD OF ATTRIBUTES?
    self.author
   end
+  
+  def no_duplicates
+    #if there's already a book with this title and brand, throw an error
+    if Book.find_by(title: title, author_id: author_id) #why author ID and not author
+      errors.add(:title, "has already been added for that author")
+    end
+  end
+
 end
 
 #not treating book as a join table
