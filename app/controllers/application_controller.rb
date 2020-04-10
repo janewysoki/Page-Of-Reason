09a05a820a5fdecf32ea88024291cfcb536f85cc
janewysoki <jane.wysoki@gmail.com>
2020-04-10 12:14:19 -0400

    Redirect user to home page if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        @current_user ||= User.find_by_id(session[:user_id])
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
+    def redirect_if_not_logged_in
+        redirect_to login_path if !logged_in?
+    end
 end
