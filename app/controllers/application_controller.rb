0f234f7a620b2f46a3043a4e3262cd7fe5c0dbbb
janewysoki <jane.wysoki@gmail.com>
2020-04-10 12:09:55 -0400

    Create method to check that user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
     private
     
+    def logged_in?
+        !!session[:user_id]
+    end
+
     def current_user
         @current_user ||= User.find_by_id(session[:user_id])
     end
