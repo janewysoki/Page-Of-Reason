6433793e03f28e844c8023bd4afc8df2bfa15c1b
janewysoki <jane.wysoki@gmail.com>
2020-04-18 19:16:53 -0400

    Add further styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-    def render_nav_bar
-        if logged_in?
-          render partial: 'layouts/logged_in_links'
-        else
-          render partial: 'layouts/logged_out_links'
-        end
-    end
 end
