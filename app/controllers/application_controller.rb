253c9506abf73652b2f76ccd6cc1a55b0afcf147
janewysoki <jane.wysoki@gmail.com>
2020-04-18 15:45:20 -0400

    Style nav bar and add picture to welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,4 @@ class ApplicationController < ActionController::Base
     def redirect_if_not_logged_in
         redirect_to login_path if !logged_in?   #how do i know when to use this?
     end
-
-    def authorized_to_edit?(book) 
-        book.user == current_user 
-    end
 end
